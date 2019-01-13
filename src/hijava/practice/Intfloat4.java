6f6862b440fdc72460ee481d6ab34c0c5200fd5f
LeeNaKyung2 <tudgus123@naver.com>
2019-01-13 13:50:47 +0900

    연습문제

---
+++ src/hijava/practice/Intfloat4.java
@@ -0,0 +1,21 @@
+package hijava.practice;
+
+public class Intfloat4 {
+
+	public static void main(String[] args) {
+		int i=10;
+		double d=2.0;
+		
+		int r1= (int)(i + d);
+		int r2= (int)(i - d);
+		int r3= (int)(i * d);
+		int r4= (int)(i / d);
+		
+		System.out.println(r1);
+		System.out.println(r2);
+		System.out.println(r3);
+		System.out.println(r4);
+
+	}
+
+}
