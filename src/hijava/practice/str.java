1e6e2ea3ffd66cb5f181c308843ba10644fe3c05
heedeandean <7942ahj@hanmail.net>
2019-01-13 13:32:07 +0900

    111

--- src/hijava/practice/str.java
+++ src/hijava/practice/str.java
@@ -3,7 +3,7 @@ package hijava.practice;
 public class str {
 
 	public static void main(String[] args) {
-		char c='한';
+		char c='한'; //11
 		byte b='B';
 		System.out.println(c);
 		System.out.println((int)c);
